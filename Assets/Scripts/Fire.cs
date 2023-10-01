ed4043dcbaa812a4f31d4c22fbb7f4deecf2e41c
Hyunwc <xogus565@naver.com>
2023-10-02 00:23:30 +0900

    플레이어 HP와 불 구현 10-02

---
+++ Assets/Scripts/Fire.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Fire : MonoBehaviour
+{
+    public float damage = 10; //데미지 양
+    public float damageInterval = 1.0f; //데미지를 입히는 간격
+
+    private float lastDamageTime = 0f; //마지막으로 데미지를 입힌 시간
+
+    public void OnTriggerStay(Collider other)
+    {
+        if(other.CompareTag("Player")) //플레이어와 충돌했을때
+        {
+            //충돌 오브젝트가 PlayerHp 스크립트를 가지고 있다면
+            PlayerHp playerHp = other.GetComponent<PlayerHp>();
+            if(playerHp != null)
+            {
+                //마지막 데미지 시간을 비교하여, 간격보다 더 지난 경우에만 데미지를 입히도록
+                if(Time.time - lastDamageTime >= damageInterval)
+                {
+                    //Damaged 메소드 호출하여 damage(10) 전달
+                    playerHp.Damaged(damage);
+                    lastDamageTime = Time.time;
+                }
+                
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerHp.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHp : MonoBehaviour
+{
+    public float hp = 100; //플레이어 체력
+    public Slider hpBar; //플레이어의 체력바
+    // Start is called before the first frame update
+    void Start()
+    {
+        hpBar.value = hp; //시작 시 체력바에 초기 체력 값 적용
+    }
+
+    public void Damaged(float damage)
+    {
+        //공격 받은 데미지만큼 체력 감소
+        hp -= damage;
+        //체력바에 체력 표시
+        hpBar.value = hp;
+    }
+    
+}
