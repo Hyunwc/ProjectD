1de5b295c5f7ea5f8b53acf61599baa6779dce19
Hyunwc <xogus565@naver.com>
2023-11-14 01:08:07 +0900

    11-14 소화기 수정완료
    완료

--- ShootingGame/Assets/Scripts/Fire.cs
+++ ShootingGame/Assets/Scripts/Fire.cs
@@ -9,23 +9,41 @@ public class Fire : MonoBehaviour
 
     private float lastDamageTime = 0f; //마지막으로 데미지를 입힌 시간
 
+    ParticleSystem ps;
+    List<ParticleSystem.Particle> inside = new List<ParticleSystem.Particle>();
+
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
     public void OnTriggerStay(Collider other)
     {
-        if(other.CompareTag("Player")) //플레이어와 충돌했을때
+        if (other.CompareTag("Player")) //플레이어와 충돌했을때
         {
             //충돌 오브젝트가 PlayerHp 스크립트를 가지고 있다면
             PlayerHp playerHp = other.GetComponent<PlayerHp>();
-            if(playerHp != null)
+            if (playerHp != null)
             {
                 //마지막 데미지 시간을 비교하여, 간격보다 더 지난 경우에만 데미지를 입히도록
-                if(Time.time - lastDamageTime >= damageInterval)
+                if (Time.time - lastDamageTime >= damageInterval)
                 {
                     //Damaged 메소드 호출하여 damage(10) 전달
                     playerHp.Damaged(damage);
                     lastDamageTime = Time.time;
                 }
-                
+
             }
+        }
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.CompareTag("Water"))
+        {
+            Debug.Log($"Effect Collision : {other.name}");
+            //공격 받은 데미지만큼 체력 감소
+            Destroy(gameObject);
         }
     }
 }
