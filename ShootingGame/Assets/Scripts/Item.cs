40833048a824b53fb6b5471cf5945cfcf6a74a84
KimYoungHoon2 <102773943+KimYoungHoon2@users.noreply.github.com>
2023-11-14 02:11:00 +0900

    맵 및 소리
    맵 수정 및 추가, 시작 이미지 추가, 비상벨 소리 추가

--- ShootingGame/Assets/Scripts/Item.cs
+++ ShootingGame/Assets/Scripts/Item.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Item", menuName = "New Item/item")]
 public class Item : ScriptableObject
 {
+
     public enum ItemType // 아이템 유형
     { 
         Used, // 소모품
         Ingredient, // 재료
         ETC, // 기타 
+        AudioItem    // 추가: 오디오 아이템
     }
 
     public string itemName; // 아이템 이름
     public ItemType itemType; // 아이템 유형
     public Sprite itemImage; // 아이템 이미지 (인벤토리)
     public GameObject itemPrefab; // 아이템 프리팹(생성시 프리팹)
+    public AudioClip itemSound; // 추가: 아이템 소리
 
 }
