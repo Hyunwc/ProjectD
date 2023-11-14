a8fc3e3dfb65a6881cf472042425c67568d7030b
chaeun1340 <103154502+chaeun1340@users.noreply.github.com>
2023-11-14 15:43:43 +0900

    11 14 소화기 패널 수정 중 2
    캔버스 plane 확인

--- ShootingGame/Assets/Scripts/ItemPickUp.cs
+++ ShootingGame/Assets/Scripts/ItemPickUp.cs
@@ -6,33 +6,4 @@ using static Item;
 public class ItemPickUp : MonoBehaviour
 {
     public Item item; // cs 에셋 할당용
-    private AudioSource itemAudio; // 아이템의 AudioSource 컴포넌트
-
-    void Start()
-    {
-        Debug.Log("ItemPickUp 스크립트의 Start 메서드 호출");
-        itemAudio = GetComponent<AudioSource>(); // 아이템의 AudioSource 가져오기
-    }
-
-    private void PlayItemSound()
-    {
-        Debug.Log("PlayItemSound() 호출");
-
-        if (itemAudio != null && item.itemType == ItemType.AudioItem)
-        {
-            Debug.Log("itemAudio 및 ItemType.AudioItem 확인");
-            AudioClip itemSound = item.itemSound;
-
-            if (itemSound != null)
-            {
-                Debug.Log("itemSound 확인");
-                itemAudio.clip = itemSound;
-                itemAudio.Play();
-            }
-            else
-            {
-                Debug.LogError("아이템 소리를 찾을 수 없습니다.");
-            }
-        }
-    }
 }
